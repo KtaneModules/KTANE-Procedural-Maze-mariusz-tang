ee3ba9be371d9d25f065cfc957069622d3b28df6
TheKuroEver <mariusztang@outlook.com>
2023-06-24 21:23:18 +0100

    Button animations

--- Assets/_ProceduralMaze/_Scripts/ArrowButton.cs
+++ Assets/_ProceduralMaze/_Scripts/ArrowButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using KModkit;
 using UnityEngine;
 
-[RequireComponent(typeof(KMSelectable))]
+[RequireComponent(typeof(KMSelectable), typeof(Animator))]
 public class ArrowButton : MonoBehaviour {
 
     [SerializeField] private MazeDirection _direction;
@@ -11,7 +11,22 @@ public class ArrowButton : MonoBehaviour {
     public KMSelectable Selectable { get; private set; }
     public MazeDirection Direction { get { return _direction; } }
 
+    private KMAudio _audio;
+    private Animator _animator;
+
     public void Awake() {
         Selectable = GetComponent<KMSelectable>();
+        Selectable.OnInteract += () => {
+            _animator.SetBool("IsPressed", true);
+            _audio.PlaySoundAtTransform("ButtonPress", transform);
+            return true;
+        };
+        Selectable.OnInteractEnded += () => {
+            _animator.SetBool("IsPressed", false);
+            _audio.PlaySoundAtTransform("ButtonRelease", transform);
+        };
+
+        _animator = GetComponent<Animator>();
+        _audio = GetComponentInParent<KMAudio>();
     }
 }
